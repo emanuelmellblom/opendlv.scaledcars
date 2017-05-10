cb145962b835e6eb9a9dac1b29a231c176941496
funkyfisch <funkyfisch@gmail.com>
2017-05-10 16:24:31 +0200

    comments more comments

--- Arduino/T4_car/T4_car.ino
+++ Arduino/T4_car/T4_car.ino
@@ -38,20 +38,22 @@ float value;
 float speeds;
 
 // For speed
-unsigned long lastDist;
-unsigned long lastTime;
+unsigned long lastDist; // measures the previous movements
+unsigned long lastTime; //  and stores them globally just to get the remainder which is the last distance and time for that traveled
 
 
 SharpIR frontRightIrSensor(GP2YA41SK0F,frontRightIrPin);
 SharpIR backRightIrSensor(GP2YA41SK0F,backRightIrPin );
 SharpIR backIrSensor(GP2YA41SK0F,backIrPin);
 //QTRSensorsRC((unsigned char[]){6}, 1, 4, QTR_NO_EMITTER_PIN);
-//QTRSensorsRC qtra((unsigned char[]) {6},1, 100, 255); 
+//QTRSensorsRC qtra((unsigned char[]) {6},1, 100, 255);
 //QTRSensorsAnalog qtra((unsigned char[]) {6}, 1, 2000, QTR_NO_EMITTER_PIN);
 //unsigned int sensorValues[1];
 
 
-
+/*
+reads a byte from serial port, current speed and angle , reads them as bytes and tranforms them into floats. used when you get info from the proxy
+*/
 
 void byteDecode(int byteIn, float *speed, float * angle) {
   float tempAng = *angle;
@@ -61,13 +63,13 @@ void byteDecode(int byteIn, float *speed, float * angle) {
   if (*angle == 48) {
     //command='a';
     *angle = tempAng;
-
   }
-
-
-
 }
 
+
+/*
+you set how many times you want to get readings from a certain sensor, adds them as a sum then averages them out
+*/
 int readSensor(int readings,SharpIR sensor){
 	//int arr[readings];
 	int x=0;
@@ -81,15 +83,15 @@ int readSensor(int readings,SharpIR sensor){
 
 void setup() {
   //	command='c';
-	speeds = 0;
-	value = 90;
+	speeds = 0; //speed of vehicle
+	value = 90; //steering angle
 	//Inputs and outputs
 	pinMode(led_lights, OUTPUT);
 	pinMode(LED_BUILTIN, OUTPUT);
 	pinMode(odometer, INPUT);
 	pinMode(rcController, INPUT);
 	pinMode(speedPin, INPUT);
-  
+
 
   //Servos
 	esc.attach(10);
@@ -99,7 +101,7 @@ void setup() {
 	car = new RcCar(MAXFORWSPEED * 10, 1400, MAXREVSPEED * 10, 1100, 1380, esc, steering);
   //esc.writeMicroseconds(50);
 
-	Wire.begin();
+	Wire.begin(); // I2C comms
 	//Serial
 	Serial2.begin(9600);
 	Serial.begin(9600);
@@ -108,14 +110,13 @@ void setup() {
 	/* for (int i = 0; i < 100; i++){
     	qtra.calibrate();       // reads all sensors 10 times at 2.5 ms per six sensors (i.e. ~25 ms per call)
   	}*/
-
-
 }
 
 
 
 
-
+/*
+reads from the integrated  circuit that returns an int from master and has all info on slaves */
 int readI2C(int address) {
 	//Serial.println(String(Wire.requestFrom(address,1)));
 	Wire.requestFrom(address, 1);
@@ -133,7 +134,7 @@ int readI2C(int address) {
 }
 
 //Ultrasonic controllers
-unsigned long tick = 0;
+unsigned long tick = 0; //measuring how often you use the US
 boolean ping = true;
 char sensorId = 0;
 
@@ -143,21 +144,21 @@ char I2C[2] = {ultrasonic_Front_Center, ultrasonic_Front_Side};
 char SensorData[7] = {0, 0, 0, 0, 0, 0, 0};
 
 
-void loop() {		
+void loop() {
 
 unsigned long times = millis();
-	
-		int n=0;
+
+		int n=0; //data average of a reading set from frontIRsensor, save it in the array, then read again from backRightIrSensorIRsensor then a last time for backIRsensor
 		n=readSensor(50,frontRightIrSensor);
 		char distanceFrontRightIr = ((n < 20 ? (n) : 0) / 2 & 31) | (5 << 5); //Calculate the distance in centimeters and store the value in a variable
 		//distanceFrontRightIr |=	5 << 5;
 		SensorData[5] = distanceFrontRightIr;
-	 	
+
 	 	n=readSensor(50,backRightIrSensor);
 		char distanceBackRightIr = ((n<20 ? (n) : 0) / 2 & 31) | (1 << 5);
 		//distanceBackRightIr |=	1 << 5;
 		SensorData[1] = distanceBackRightIr;
-	 	
+
 		n=readSensor(50,backIrSensor);
 		char distanceBackIr = ((n < 20 ? ( n) : 0) / 2 & 31) | (4 << 5);
 		//distanceBackIr|=(4 << 5);
@@ -165,6 +166,7 @@ unsigned long times = millis();
 		SensorData[4] = distanceBackIr;
 
     // Read once approx every 500ms
+    // read the distance traveled every 500ms
     if(times - lastTime > 500)
     {
       unsigned long dist = encoder.getDistance();
@@ -179,26 +181,22 @@ unsigned long times = millis();
 
 	if (ping) {
 		Wire.beginTransmission(I2C[sensorId]);
-		Wire.write(0x02);
-		Wire.write(14);
+		Wire.write(0x02); // setthe measuring distance in the register two then puts a value to the register
+		Wire.write(14); //
 		Wire.endTransmission(1);
 		//delay(20);
-		
+
 		//delay(20);
 		Wire.beginTransmission(I2C[sensorId]);
-		Wire.write(0x00);
-		Wire.write(0x51);;
+		Wire.write(0x00); //
+		Wire.write(0x51); //
 		Wire.endTransmission(1);
 		tick = millis();
 		ping = false;
-		Wire.write(0x51);;
-		Wire.endTransmission(1);
 		tick = millis();
-		ping = false;
-		
 	}
 
-
+/* The below changes them in order to read from them */
 	if (!ping && millis() - tick > 5) {
 		Wire.beginTransmission(I2C[sensorId]);
 		Wire.write(0x03);
@@ -209,7 +207,6 @@ unsigned long times = millis();
 			times=millis();
 		}*/
 		SensorData[2 + sensorId] = ((v <= 50 && v != 0 ? v : 0) / 2 & 31) | ((2+sensorId)<<5);
-		
 		/*if(sensorId==0)
 			{Serial.println((int)( SensorData[2 + sensorId]&31)*2);
 			times=millis();
@@ -219,52 +216,53 @@ unsigned long times = millis();
 	}
 
 	for (int i = 1; i < sizeof(SensorData); i++) {
-	
+
 		Serial.print(SensorData[i]);
 		Serial2.println(String((SensorData[i]&31)*2)+" id "+String((SensorData[i]>>5)&7));
 	}
 
 
 
+/* controller stuff here */
 	unsigned int steeringCH = pulseIn(rcController, HIGH);
 		//steering.write(steeringCH);
 		//min <90   max >180
 	unsigned int speedCH = pulseIn(speedPin, HIGH);
 	//esc.write(speedCH);
 
+
+/* reading from proxy and bluetooth feedback */
 	//reRead:
 	if (Serial.available()) {
 		char k = Serial.read();
 		byteDecode(k, &speeds, &value);
 		Serial2.println("angle is"+String(value));
+    //Serial2.println("speed is"+String(speeds));
 		// Serial.println(k);
 		car->setAngle(value + 120);
-    
+
 	}else {
 		//Serial2.println("not available####3");
 
 	}
 
-	
+/*filter out input from rc controller*/
+/*set speed from rc controller*/
 	if (speedCH / 10 < 190) {
 		car->setRawSpeed(&speedLimit, speedCH);
 	}else {
-
 		car->setSpeed(car->NEUTRAL);
 		//command='c';
 		car->setAngle(90);
 	}
-
 	Serial2.println(" delay is"+String(millis()-times));
-
-
 }
+/* proxy input is : -60 to 60 for steering angle, 5 bits of which are steering info 3 bits are ms. the frist three bytes should show the speed */
 
 
 
 
-
-
+/*set global speed limit for safety*/
 int speedLimit(int speed) {
 	if (speed / 10 < 140 && speed / 10 > 130) { //if in neutral
 		return 1387;
@@ -276,7 +274,6 @@ int speedLimit(int speed) {
 	}else {
 		return speed;
 	}
-
 }
 
 
@@ -292,5 +289,3 @@ bool isLetter(char c) {
 	return c >= 92 && c <= 122;
 
 }
-
-
