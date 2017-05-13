dc40c62f2cf7790501bbfb0fbae3e9fc75e052d6
ostkaka <ost@airmail.cc>
2017-05-13 12:18:32 +0200

    added a specific buffer for the odometer SerialRead keeps track of the travled distance and push that data via the new buffer "odoMem" which guarantees that the data will only be read once

--- code/proxy/include/SerialRead.h
+++ code/proxy/include/SerialRead.h
@@ -1,12 +1,18 @@
 #ifndef SerialRead_H_
 #define SerialRead_H_
 
+#define ODOMETER 6
 
 #include <iostream>
 #include <opendavinci/odcore/io/StringListener.h>
 #include <opendavinci/odcore/base/Thread.h>
+#include <opendavinci/odcore/base/Lock.h>
+#include <opendavinci/odcore/wrapper/SharedMemory.h>
+#include <opendavinci/odcore/wrapper/SharedMemoryFactory.h>
 #include <string>
 
+using namespace odcore;
+using namespace odcore::wrapper;
 
 	class SerialRead :public odcore::io::StringListener {
 		public:
@@ -16,9 +22,13 @@
 			std::string readstr();
 			char get(int);
 		private:
-			std::string output;
+			//std::string output;
 			char buff[8];
+			unsigned long odometerCounter;
+			std::shared_ptr<SharedMemory> odoMem;
+			void incrementOdometer(char);
 		};
+
 
 
 #endif /*PROXY_H_*/
