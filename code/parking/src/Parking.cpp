2c06ffb83a099882019151a6bc37a862d24f11e7
funkyfisch <funkyfisch@gmail.com>
2017-05-15 19:04:19 +0200

    record breaker

--- code/parking/src/Parking.cpp
+++ code/parking/src/Parking.cpp
@@ -112,7 +112,7 @@ namespace scaledcars {
 
 	void Parking::sendMotionData(double steeringAngle, int speed) {
 
-		cerr << "steeringAngle = " << steeringAngle << endl;
+		//cerr << "steeringAngle = " << steeringAngle << endl;
 		char output = 0x00;
 		output = (((int)(steeringAngle) / 4) + 15) & 31;
 
@@ -146,81 +146,100 @@ namespace scaledcars {
 				double parkTimer = 0;
 				sendMotionData(0, 3);
 				bool parked = false;
-        while (getModuleStateAndWaitForRemainingTimeInTimeslice() == odcore::data::dmcp::ModuleStateMessage::RUNNING) {
-                //change these as needed- stage 2 and 4 should have same value for it to end up in a parallel position
-                // const int stage1 = 200; // time needed for backing a bit
-                // const int stage2 = 1200; //time needed for first curve
-                // const int stage3 = 800; // time needed for the straight backwards (45 degree inclination to the lane) ( this is only needed if there is not enough y displacement)
-                // const int stage4 = 1200; // time needed for second curve
-                // const int stage5 = 500;
-								// const int stage6 = 500;
-								// working values above without extra stages for 85cm parking space
-								// time needed for going straight back- later replace that with sensor reading telling it when to stop
-								//optional stages for more turning
-								//const int stage6,7,8 etc
-								const int stage1 = 200; // adjust this when parking space is found //initially 200
-                const int stage2 = 2400; //time needed for first curve	//initially 2200
-                const int stage3 = 0; //initially 200
-                const int stage4 = 1200; ////initially 1000// time needed for second curve
-                const int stage5 = 1200;//initially 1200
-								const int stage6 = 1000;//initially 1000
-                //const int32_t ULTRASONIC_FRONT_CENTER = 2;
-                //const int32_t ULTRASONIC_FRONT_RIGHT = 3;
-                const int32_t INFRARED_FRONT_RIGHT = 5;
-                const int32_t INFRARED_REAR_RIGHT = 1;
-                const int32_t INFRARED_BACK = 4;
-                const int32_t ODOMETER = 6;
+				//change these as needed- stage 2 and 4 should have same value for it to end up in a parallel position
+				// const int stage1 = 200; // time needed for backing a bit
+				// const int stage2 = 1200; //time needed for first curve
+				// const int stage3 = 800; // time needed for the straight backwards (45 degree inclination to the lane) ( this is only needed if there is not enough y displacement)
+				// const int stage4 = 1200; // time needed for second curve
+				// const int stage5 = 500;
+				// const int stage6 = 500;
+				// working values above without extra stages for 85cm parking space
+				// time needed for going straight back- later replace that with sensor reading telling it when to stop
+				//optional stages for more turning
+				//const int stage6,7,8 etc
+				KeyValueConfiguration kv = getKeyValueConfiguration();
 
-								if (currentSpaceSize <= 80) {
+				const int stage0 = kv.getValue<int32_t>("parking.stage0");//100;
+				const int stage1 = kv.getValue<int32_t>("parking.stage1");//400; // adjust this when parking space is found //initially 200
+				const int stage2 = kv.getValue<int32_t>("parking.stage2");//2400; //time needed for first curve	//initially 2200
+				const int stage3 = kv.getValue<int32_t>("parking.stage3");//0; //initially 200
+				const int stage4 = kv.getValue<int32_t>("parking.stage4");//1200; ////initially 1000// time needed for second curve
+				const int stage5 = kv.getValue<int32_t>("parking.stage5");//1200;//initially 1200
+				const int stage6 = kv.getValue<int32_t>("parking.stage6");//1000;//initially 1000
+				const int stage7 = kv.getValue<int32_t>("parking.stage7");//
+				const int stage8 =kv.getValue<int32_t>("parking.stage8");//
+				const int stage9 =kv.getValue<int32_t>("parking.stage9");//
+				const int minimumTimeToFindSpace = kv.getValue<int32_t>("parking.minimumTimeToFindSpace");//6500;
+
+
+				//const int32_t ULTRASONIC_FRONT_CENTER = 2;
+				//const int32_t ULTRASONIC_FRONT_RIGHT = 3;
+				const int32_t INFRARED_FRONT_RIGHT = 5;
+				const int32_t INFRARED_REAR_RIGHT = 1;
+				const int32_t INFRARED_BACK = 4;
+				sendMotionData(0,3);
+				//const int32_t ODOMETER = 6;
+
+        while (getModuleStateAndWaitForRemainingTimeInTimeslice() == odcore::data::dmcp::ModuleStateMessage::RUNNING) {
+
+
+
+								TimeStamp currentTime;
+								double deltaTime = (currentTime.toMicroseconds() - lastTime.toMicroseconds())/1000;
+								lastTime = currentTime;
+
+								if (currentSpaceSize <= minimumTimeToFindSpace) {
 									cerr << "Searching" << endl;
 	                // Go forward.
-									sendMotionData(0, 4);
+									sendMotionData(0, 5);
 
 	                // Get odometer value - probably approx in cm
-	                currentSpaceSize += readSensorData(ODOMETER);
-									cerr << currentSpaceSize << endl;
+	                //currentSpaceSize += readSensorData(ODOMETER);
+									currentSpaceSize += deltaTime;
+
 	                // Check if an object is blocking the space.
 	                // If it is, reset space size
 	                int rear = readSensorData(INFRARED_REAR_RIGHT);
 	                int front = readSensorData(INFRARED_FRONT_RIGHT);
-	                if((rear < 15 && rear != 0) || (front < 15 && front != 0)){
+	                if((rear < 20 && rear != 0) && (front < 20 && front != 0)){
 	                    cerr << "Object detected" << endl;
 	                    currentSpaceSize = 0;
+											cerr << "reset space : " << currentTime.toMicroseconds()/1000<< endl;
 	                }
-
-	                cerr << "Back IR: " << rear << endl;
-	                cerr << "Front IR: " << front << endl;
-
 								}
 
-
-								else if (currentSpaceSize > 80) {
-
-									TimeStamp currentTime;
-			            double deltaTime = (currentTime.toMicroseconds() - lastTime.toMicroseconds())/1000;
-			            lastTime = currentTime;
+								else if (currentSpaceSize > minimumTimeToFindSpace && parked == false) {
+									cerr<<currentTime.toMicroseconds()/1000<<endl;
 									parkTimer += deltaTime;
-
-									while (parked == false) {
-										if (parkTimer < stage1) {
-				              sendMotionData(0, 2);
+									cerr << parkTimer << endl;
+										if (parkTimer < stage0) {
+											sendMotionData(0,3);
+										}
+										else if (parkTimer < stage0+stage1) {
+				              sendMotionData(0, 5);
 				            }
-				            else if (parkTimer < stage1+stage2) {
+				            else if (parkTimer < stage0+stage1+stage2) {
 				              sendMotionData(60, 2);
 				            }
-				            else if (parkTimer < stage1+stage2+stage3) {
+				            else if (parkTimer < stage0+stage1+stage2+stage3) {
 				              sendMotionData(0, 2);
 				            }
-				            else if (parkTimer < stage1+stage2+stage3+stage4) {
+				            else if (parkTimer < stage0+stage1+stage2+stage3+stage4) {
 				              sendMotionData(-60, 2);
 				            }
-				            else if (parkTimer < stage1+stage2+stage3+stage4+stage5) {
+				            else if (parkTimer < stage0+stage1+stage2+stage3+stage4+stage5) {
 				              sendMotionData(60, 4);
 				            }
-				            else if (parkTimer < stage1+stage2+stage3+stage4+stage5+stage6){
+				            else if (parkTimer < stage0+stage1+stage2+stage3+stage4+stage5+stage6){
 				              sendMotionData(-60, 2);
 				            }
-										else if(parkTimer > stage1+stage2+stage3+stage4+stage5+stage6 && parked == false) {
+										else if (parkTimer <  stage0+stage1+stage2+stage3+stage4+stage5+stage6+stage7 ) {
+											sendMotionData(60, 4);
+										}
+										else if (parkTimer <  stage0+stage1+stage2+stage3+stage4+stage5+stage6+stage7+stage8 ) {
+											sendMotionData(-60, 2);
+										}
+										else if(parkTimer > stage0+stage1+stage2+stage3+stage4+stage5+stage6+stage7+stage8+stage9 && parked == false) {
 											int back = readSensorData(INFRARED_BACK);
 											if (back > 10 || back == 0) {
 												sendMotionData(0, 2);
@@ -232,7 +251,7 @@ namespace scaledcars {
 										else {
 												sendMotionData(0,3);
 										}
-									}
+
 
 								}
         }
